d95480edc2f812d0225a24ed4219b8ffebe8b4ec
Ping-Lin Chang <pinglin02@gmail.com>
2012-02-04 13:52:59 +0000

    render using GL_LINEAR

--- src/stereo_calib.cpp
+++ src/stereo_calib.cpp
@@ -327,8 +327,8 @@ void StereoCalibration::InitTexture()
     glBindTexture(GL_TEXTURE_2D, ChessTexID);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
     glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, 8*calib_params.BoardSize.width,
                  8*calib_params.BoardSize.height, 0, GL_RGBA, GL_UNSIGNED_BYTE,
                  checkImage);
